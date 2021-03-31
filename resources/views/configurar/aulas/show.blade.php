9be88e31ce114c3adc4495a96f1d0397de5fa77f
amaiafilo <4m4i4@users.noreply.github.com>
2021-03-31 16:28:46 +0200

    php al controlador

--- resources/views/configurar/aulas/show.blade.php
+++ resources/views/configurar/aulas/show.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
-  @section('tablas')
-      @php
+  @section('etapaUso')
+  @php
+  use App\Models\Mesa;
+      $mesas = Mesa::all();
+  @endphp
+      {{-- @php
         use App\Models\Estudiante;
         use App\Models\Mesa;
         use App\Models\Materia;
         use Illuminate\Support\Arr;          
         $mesas = Mesa::all();
+        $hasMesas = $aula->mesas;
+        // dd( $mesas);
         $n_col = $aula->num_columnas;
         $n_fila = $aula->num_filas;
         $user = auth()->user()->id;
@@ -18,14 +24,13 @@
         $estudiantes = $materia->estudiantes;
         // dd($estudiantes);
         $n_student = $estudiantes->count();
-
         $contador = 0;
             
       @endphp
             
-      <div class="bg-white ssss_aula">
+      <div class="flex justify-center">
         <div class="grid grid-cols-{{$n_col}}">
-          @if($aula_hasMesas == null) Si no hay mesas en el aula las añadimos
+          @if($aula_hasMesas == null) Si no hay mesas recarga la página
             @for ($i = $n_fila;  $i > 0; $i--) 
               @for ($ii = 1; $ii <= $n_col; $ii++)
                 @php
@@ -53,6 +58,7 @@
               //   dd($randomList);
               $n_mesas = $aula->num_mesas;
               $dif = $n_mesas - $n_student;
+              // dd($hasMesas);
               $estaMesa = Mesa::where('aula_id',$aula->id)->get();
               $firstMesa = $estaMesa[0]->id;
               $lastMesa = $firstMesa + $estaMesa->count();   
@@ -83,31 +89,41 @@
               } 
             @endphp
           @endif
-        </div>
-              
-        <div class="grid grid-cols-{{$n_col}}">
+        </div> --}}
+         <hr class="h-2">  
+         <div class="bg-666 w-100 h-100 mx-auto">  
+        <div class="grid grid-cols-{{$aula->num_columnas}} mx-4 p-6 grid-rows-{{$aula->num_filas}} h-90  ">
+            {{-- {{dd( $hasMesas)}} --}}
+            {{-- {{dd( $mesas)}} --}}
           @foreach ($mesas->where('aula_id', $aula->id) as $mesa)
-            <div id={{$mesa->id}} class="mesa text-center" title="mesa_{{$mesa->id}}">
+            <div id={{$mesa->id}} class="mesa text-center flex-column justify-center" title="mesa_{{$mesa->id}}">
               @if($mesa->is_ocupada == true)
                 <div>       
-                  <button id="A_bt_{{$mesa->id}}" class="bt_mesa propiedad_A bg-yellow" title="A_bt_{{$mesa->id}}" onclick="this.classList.add('active')">A</button>
-                  <button id="B_bt_{{$mesa->id}}" class="bt_mesa propiedad_B bg-sky f_right" title="B_bt_{{$mesa->id}}" onclick="suma(B_bt_{{$mesa->id}},10)">0</button>
+                  <button id="A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}" 
+                    class="bt_mesa propiedad_A bg-sobreB"
+                    title="A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}"
+                    {{-- onmousedown="this.classList.toggle('bg-sobreB')" --}}
+                    onclick="sino(A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}),lee(A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}})">Sí</button>
+                  <button id="B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}" 
+                    class="bt_mesa propiedad_B bg-sky f_right" 
+                    title="B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}"
+                    onclick="suma(B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}},10),lee('B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}')">0</button>
                 </div>
-                <div class="nombre_mesa">
+                <div class="">
                   <button id="name_{{$mesa->id}}" class="nombre_mesa d_block py-0" title=" id: {{$mesa->estudiante->id}}" onclick= "desabilita({{$mesa->id}})">{{$mesa->estudiante->nombre}} {{Str::limit($mesa->estudiante->apellidos, 1)}}</button>
                 </div> 
               @else
                 <div>       
-                  <button id="A_bt_{{$mesa->id}}" class="bt_mesa propiedad_A " disabled title="A_bt_{{$mesa->id}}"></button>
-                  <button id="B_bt_{{$mesa->id}}" class="bt_mesa propiedad_B f_right" disabled title="B_bt_{{$mesa->id}}"></button>
+                  <button id="A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}" class="bt_mesa propiedad_A " disabled title="A_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}"></button>
+                  <button id="B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}" class="bt_mesa propiedad_B f_right" disabled title="B_bt_{{$mesa->id}}_{{$mesa->columna}}_{{$mesa->fila}}"></button>
                 </div>
                 <div class="nombre_mesa" disabled>
-                    <button class="nombre_mesa d_block py-0"  title=" disabled: {{$mesa->id}}" disabled>-- --</button>
+                  <button class="nombre_mesa d_block py-0" title=" disabled: {{$mesa->id}}" disabled>-- --</button>
                 </div>
               @endif
             </div>    
           @endforeach
-
+</div> 
           {{-- @for($i = $firstMesa; $i < $lastMesa; $i++)
 
             <div class="mesa text-center" title="mesa_{{$i}}">
@@ -154,8 +170,6 @@
       </div> --}}
     <!-- Scripts -->
 
-  {{-- <script src="{{ asset('js/app.js') }}" type="text/js"></script> --}}
-  {{-- <script src="{{ asset('js/custom.js') }}" type="text/js"></script>--}}
       <script>
           // function vaciasModal(valor_id){
           //   let ar_id = valor_id.split('_');
@@ -181,10 +195,39 @@
 }
 
   function suma(x, y = 1){
-    let res = x.innerHTML;
+  
+    
+       let res = x.innerHTML;
+      let el=document.getElementById(x);
+     let valor=  el.value;
+     console.log("hola "+valor);    
     res = parseInt(res) + y;
     x.innerHTML = res;
+       
   }
-      </script> 
-{{-- </body>
-</html> --}}
+  function sino(x){
+    let res = x.innerHTML;
+    if(res=="Sí"){
+      res="No";
+      x.classList.remove('bg-sobreB');
+      x.classList.add('bg-sobreN');
+    }else if(res=="No"){
+      res="Sí";
+      x.classList.remove('bg-sobreN');
+      x.classList.add('bg-sobreB');
+      }
+
+    x.innerHTML = res;
+    console.log(res); 
+    // x.classList.toggle('bg-sobreN');
+  }
+  function lee(x){
+   let valor =document.getElementById(x).innerHTML;
+   console.log("v: "+valor);
+  let cero= parseInt(valor)%50;
+  console.log("resto: "+cero);
+  if(cero==0){
+    document.getElementById(x).classList.toggle('bg-blue');
+  }
+  }
+</script> 
