e76f84eba389d497270b9d26f3d4860e5902d413
amaiafilo <4m4i4@users.noreply.github.com>
2021-04-09 23:26:56 +0200

    centrar container

--- resources/views/botones.blade.php
+++ resources/views/botones.blade.php
@@ -1,23 +1,29 @@
+{{-- /botones --}}
 @extends('layouts.app')
-@section('tablas')
-<div class="container">
 
-  <div class="caja mx-1 px-4">
+@section('tablas')
+
+<div id="#page-botones" class="container">
+
+  {{-- <div class = ""> --}}
+
+  <div class="caja mt-4">
     <div class="caja-header">
-      <h1 class=" my-4 text-center">Personaliza Klik-Class</h1>
-      <div class="flex items-center justify-center">
-        <a class="default text-sm px-4 py-1 text-center mr-1" href="#indice_1">Elije tus Botones</a>
-        <a class="default text-sm px-4 py-1 text-center mr-1" href="#indice_2">Graduales y Combinados</a>
-        <a class="default text-sm px-4 py-1 text-center mr-1" href="#indice_3">Control de Asistencia</a>
-        <a class="default text-sm px-4 py-1 text-center" href="#indice_4">Botones Premio</a>
+      <div class="encabezamiento">
+      <h1 class="h1 mb-2 ">Personaliza<br>Klik-Class</h1></div>
+      <div class="menu-botones">
+        <a class="items-menu-botones default" href="#indice_1">Elije tus Botones</a>
+        <a class="items-menu-botones default" href="#indice_2">Graduales y Combinados</a>
+        <a class="items-menu-botones default" href="#indice_3">Control de Asistencia</a>
+        <a class="items-menu-botones default" href="#indice_4">Botones Premio</a>
       </div>
-      <p class="my-4">Esta es la página donde el usuario puede personalizar los botones. Se ofrecen modelos de uso para mostrar el funcionamiento de cada tipo. Las imágenes son ideas sobre su contenido</p>
+      <p class="text-justify px-4 my-4">Esta es la página donde el usuario puede personalizar los botones. Se ofrecen modelos de uso para mostrar el funcionamiento de cada tipo. Las imágenes son ideas sobre su contenido</p>
     </div>
   </div>
   {{-- <div class="grid grid-cols-2"> --}}
-  <div  id= "indice_1"class="caja mx-1 px-4">
+  <div  id= "indice_1"class="caja px-4">
     <div class="caja-body">
-      <h2  class="my-4 text-blue-30 smallCaps">Prueba cómo funcionan antes de elegir</h2>
+      <h2  class="my-4 text-6 text-blue-30 text-center smallCaps">Prueba cómo funcionan antes de elegir</h2>
       <hr>
      <figure>
         <img src="/images/botonesConfig.png" alt="muestrario de botones" width="994px" hight="572px">
@@ -26,14 +32,14 @@
         </figcaption>
       </figure>
       <hr>
-      <p class="my-4">Al hacer clic en el botón "Usar este botón en..." se abre un formulario con la lista para seleccionar a qué materias-grupos quiere asignarse y otras opciones de configuración. </p>
+      <p class="text-justify my-4">Al hacer clic en el botón "Usar este botón en..." se abre un formulario con la lista para seleccionar a qué materias-grupos quiere asignarse y otras opciones de configuración. </p>
     </div>
   </div>
 
-  <div  id= "indice_2"class="caja mx-1 px-4">
+  <div  id= "indice_2"class="caja px-4">
     <div class="caja-body">
 
-      <h2  class="my-4 text-blue-30 smallCaps" >Botones graduales y Combinados</h2>
+      <h2  class="my-4 text-6 text-blue-30  text-center smallCaps" >Botones graduales y Combinados</h2>
       <hr>
 
       <figure>
@@ -43,13 +49,13 @@
           </figcaption>
       </figure>
       <hr>
-      <p class="my-4">Además de los valores (inicial, final e incremento) se puede configurar también el aspecto: el color, la forma y el texto en el botón y si cambian o no..</p>
+      <p class="text-justify my-4">Además de los valores (inicial, final e incremento) se puede configurar también el aspecto: el color, la forma y el texto en el botón y si cambian o no..</p>
     </div>
   </div>
 
-  <div id= "indice_3"  class="caja mx-1 px-4">
+  <div id= "indice_3"  class="caja px-4">
     <div class="caja-body">
-      <h2  class="my-4 text-blue-30 smallCaps" >Control de la asistencia</h2>
+      <h2 class="my-4 text-6 text-blue-30  text-center smallCaps" >Control de la asistencia</h2>
       <hr>
      
       <div class="grid   md:gridcols-2 items-center justify-center">
@@ -67,14 +73,14 @@
       </figure>
       </div>
       <hr>
-      <p class="my-4">Se pueden añadir más opciones usando el control de asistencia múltiple. Por ejemplo: Asiste, Falta, Retraso, Expulsión. El segundo clic guarda 'Retraso' (y opcionalmente, cuántos minutos). El tercer clic señala una expulsión.</p>
+      <p class="text-justify my-4">Se pueden añadir más opciones usando el control de asistencia múltiple. Por ejemplo: Asiste, Falta, Retraso, Expulsión. El segundo clic guarda 'Retraso' (y opcionalmente, cuántos minutos). El tercer clic señala una expulsión.</p>
     </div>
   </div>
 
-  <div  id= "indice_4" class="caja mx-1 px-4">
+  <div  id= "indice_4" class="caja px-4">
     <div class="caja-body">
-      <h2  class="my-4 text-blue-30 smallCaps" >Botones con premio aleatorio</h2>   
-      <hr>   
+      <h2 class="my-4 text-6 text-blue-30  text-center smallCaps" >Botones con premio aleatorio</h2>   
+      <hr class="bg-azul-kc">   
         <figure>
          <img src="/images/botonesConfig03.png" alt=" botones premio" width="1000" height="566">
           <figcaption>
@@ -82,10 +88,12 @@
          </figcaption>
         </figure>
          <hr>
-      <p class="my-4">Se puede rifar un premio al finalizar la clase o establecer el periodo en que puede darse. Opcionalmente se podrían añadir restricciones como: solo se dará si no tiene puntos negativos.</p>
+      <p class="text-justify my-4">Se puede rifar un premio al finalizar la clase o establecer el periodo en que puede darse. Opcionalmente se podrían añadir restricciones como: solo se dará si no tiene puntos negativos.</p>
     </div>
 
   </div>
+
+</div>
   {{-- </div> --}}
 
 @endsection
