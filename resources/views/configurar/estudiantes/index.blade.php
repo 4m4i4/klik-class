dc1dc046004999e45b6a330a5591ecef8c6e7add
amaiafilo <4m4i4@users.noreply.github.com>
2021-04-10 05:02:19 +0200

    lang

--- resources/views/configurar/estudiantes/index.blade.php
+++ resources/views/configurar/estudiantes/index.blade.php
@@ -14,10 +14,14 @@
       <div class="caja">  <!-- CABECERA estudiantes -->
         <div class = "caja-header">
           @if (request()->is('mostrar/estudiantes/*'))
-            <div class = "grd grid-cols4 items-center">
+            <div class = "grid grid-cols4 w-100 items-center">
               <h2>{{ __('My')}} {{ __('Students')}}: ({{$estudiantes->count()}})</h2>
-              <a href="{{route('materias.index')}}" title="Volver a la página anterior " class="btn atras">
-                <span class="ico-shadow"> 👈 </span>Atrás</a>
+              <a href="{{route('materias.index')}}"
+                title="Volver a la página anterior" 
+                class="btn atras">
+                <span class="ico-shadow"> 👈 </span>
+                {{__('Previous')}}
+              </a>
               <form action="{{ route('estudiantes.borrarGrupo', $materia_id) }}" method="POST">
                 @csrf
                 @method('delete')
@@ -25,27 +29,40 @@
                     class="btn borrar" 
                     title="Borrar grupo">
                     <span class="ico-shadow"> ❌ </span>
-                    <span class="bt-text-hide">{{ __('Delete Group') }}</span>
+                    <span class="bt-text-hide">{{ __('Delete') }} {{ __('Group') }}</span>
                   </button>
               </form>
-              <form action="{{route('estudiantes.index',$materia_id)}}"  method="POST">
-              @csrf
-              <select id="materia_id" name="materia_id" value="{{ $materia_id }}" class="d_block" onsubmit="seleccionaMateria(materia_id)" >
-                @foreach ($materia as $laMateria)
-                    <option value={{$laMateria->id}} {{$laMateria->id == $materia_id? 'selected' : ''}}><a href="{{route('estudiantes.porMateria',$laMateria->id)}}" title="loquesea">{{$laMateria->materia_name}}</a></option>
-                @endforeach
-              </select> </form>
+
+              <form action="{{route('estudiantes.index',$materia_id)}}" method="POST">
+                @csrf
+                <select id="materia_id" 
+                  name="materia_id" 
+                  value="{{ $materia_id }}" 
+                  class="d_block" 
+                  onsubmit="seleccionaMateria(materia_id)">
+                  @foreach ($materia as $laMateria)
+                    <option value={{$laMateria->id}} {{$laMateria->id == $materia_id? 'selected' : ''}}>{{$laMateria->materia_name}}</option>
+                  @endforeach
+                </select>
+              </form>
             </div>
           @endif
           @if (request()->is('configurar/estudiantes'))
-            <div class = "grid grid-cols-3-fr items-center">
+            <div class = "grid grid-cols-3-fr w-100 items-center">
               <h2>{{ __('My')}} {{ __('Students')}}</h2>
-              <a href="{{route('materias.index')}}" title="Volver a la página anterior " class="btn atras">
-                <span class="ico-shadow"> 👈 </span>Atrás
+              <a href="{{route('materias.index')}}" 
+                title="Volver a la página anterior" 
+                class="btn atras">
+                <span class="ico-shadow"> 👈 </span>
+                {{__('Previous')}}
               </a>
-              <select id="materia_id" name="materia_id" value="{{ $materia_id }}" class="d_block" onchange="seleccionaMateria(materia_id)" >
+              <select id="materia_id" 
+                name = "materia_id" 
+                value = "{{ $materia_id }}" 
+                class = "d_block" 
+                onchange = "seleccionaMateria(materia_id)">
                 @foreach ($materia as $laMateria)
-                    <option value={{$laMateria->id}} {{$laMateria->id == $materia_id? 'selected' : ''}}>{{$laMateria->materia_name}}</option>
+                  <option value = {{$laMateria->id}} {{$laMateria->id == $materia_id? 'selected' : ''}}>{{$laMateria->materia_name}}</option>
                 @endforeach
               </select>
             </div>
@@ -60,7 +77,6 @@
                   <th>Id</th>
                   <th>{{ __('Name') }}</th>
                   <th>{{ __('Surnames') }}</th>
-                  {{-- <th>{{ __('Full name') }}</th> --}}
                   <th>{{ __('Subject') }}</th>
                   <th>{{ __('Edit') }}</th>
                   <th>{{ __('Delete') }}</th>
@@ -71,24 +87,24 @@
               @foreach ($estudiantes as $estudiante)
                 <tr>
                   <td><!-- -id -->
-                        {{ $estudiante->id }}
+                    {{ $estudiante->id }}
                   </td>
-                       <!-- Nombre -->
+                    <!-- Nombre -->
                   <td title="{{$estudiante->nombre_completo }}">
-                        {{$estudiante->nombre }}
+                    {{$estudiante->nombre }}
                   </td>
                   <td><!-- Apellidos -->
-                         {{$estudiante->apellidos }}
+                    {{$estudiante->apellidos }}
                   </td>
                   <td><!-- Materia -->
-                      {{$estudiante->materia->materia_name }}
+                    {{$estudiante->materia->materia_name }}
                   </td>
                   <td>
                     <a href="{{ route('estudiantes.edit', $estudiante->id) }}" 
-                        class= "btn editar mt--1" 
-                        title= "Editar estudiante id= {{ $estudiante->id }}">
-                      <span class="ico-shadow"> 📝 </span>
-                      <span class="bt-text-hide">{{ __('Edit') }}</span> 
+                      class= "editar btn" 
+                      title= "Editar estudiante id= {{ $estudiante->id }}">
+                      <span class="ico-shadow">📝 </span>
+                      <span class="bt-text-hide ">{{ __('Edit') }}</span> 
                     </a>
                   </td>
                   <td>    <!-- Borrar -->
@@ -96,10 +112,10 @@
                       @csrf
                       @method('delete')
                         <button type="submit" 
-                            class="btn borrar" 
-                            title="Borrar estudiante id= {{ $estudiante->id }}">
+                          class="borrar btn" 
+                          title="Borrar estudiante id= {{ $estudiante->id }}">
                           <span class="ico-shadow">❌ </span>
-                          <span class="bt-text-hide">{{ __('Delete') }}</span>
+                          <span class="bt-text-hide text-overflow">{{ __('Delete') }}</span>
                         </button>
                     </form>
                   </td>
@@ -111,7 +127,7 @@
         <div class="center">{{ $estudiantes->links() }}</div>
          
       </div>      <!-- fin de body-TABLA estudiantes -->
-     
+      <div class="h-8"></div>
     </div>
 
   </div>
@@ -122,7 +138,7 @@
     function seleccionaMateria(materia_id){
       let x = document.getElementById('materia_id');
       value_materia_id = x.value;
-       console.log("valor: "+value_materia_id);
+      console.log("valor: "+value_materia_id);
       document.getElementById('materia_id').value = value_materia_id;
       // return value_materia_id;
       let ipPuerto = location.host;
@@ -134,21 +150,19 @@
       // console.log("direc1 :"+ ipPuerto);
       let nuevaRuta = protocolo+"://"+ipPuerto+"/"+rutaArr[1]+"/"+rutaArr[2]+"/"+value_materia_id;
       console.log("Nueva ruta :"+ nuevaRuta);
-      fetch(`${value_materia_id}`, {
-        headers:{
-         'Content-Type': 'application/json',
-         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-         'allow':'405'
-        },
-        method:'POST',
-        body: JSON.stringify(value_materia_id)
-        })
-        .then(response => response.json())
-        .then(function(result){alert(result.message);})
-        .catch(function (error){console.log("error");});
+      // fetch(`${value_materia_id}`, {
+      //   headers:{
+      //    'Content-Type': 'application/json',
+      //    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+      //    'allow':'405'
+      //   },
+      //   method:'POST',
+      //   body: JSON.stringify(value_materia_id)
+      //   })
+      //   .then(response => response.json())
+      //   .then(function(result){alert(result.message);})
+      //   .catch(function (error){console.log("error");});
       //location.replace(nuevaRuta)
     }
-
-
   </script>
 @endsection
