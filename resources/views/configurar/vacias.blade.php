cb6dc22a8ee81274682b49a4ff3f499c5efa8985
amaiafilo <4m4i4@users.noreply.github.com>
2021-04-27 20:35:22 +0200

    cambiar mesas funciona

--- resources/views/configurar/vacias.blade.php
+++ resources/views/configurar/vacias.blade.php
@@ -7,17 +7,23 @@
   @include('include.formBanner')
     <div>
       <div class="px-6 caja-header text-center">
-        <h3>Cambiar mesas del aula<br></h3>
+        <h3>Cambiar mesas del aula</h3>
       </div>
       <form class="px-6" method="POST" action="{{route('aulas.updateMesasVacias', $aula->id) }}" method="POST" >
       @csrf 
       @method('PUT')  
       <div class="">
         <div class="mb-2"><!-- $clase->user_id  -->
-          @php $id=[]; @endphp 
+          @php $idss=[]; @endphp 
             {{-- <p>Usuari@: {{auth()->user()->name}}</p> --}}
-            {{-- <p>Aula: {{$aula->aula_name}}</p> --}}
-          <p class="ejemplo">{{$materia}}: <strong>{{$aula->num_columnas}}</strong> columnas y <strong>{{$aula->num_filas}}</strong> filas;<br> <strong>{{$estudiantes->count()}}</strong> estudiantes y <strong>{{$vacias->count()}}</strong> mesas vacías.</p> 
+          <p class="text-center"> {{$aula->aula_name}}</p>
+          <p class="ejemplo">
+            {{-- {{$materia}}:  --}}
+            <strong>{{$aula->num_columnas}}</strong> columnas y 
+            <strong>{{$aula->num_filas}}</strong> filas;<br> 
+            <strong>{{count($ids_estudiante)}}</strong> estudiantes y 
+            <strong>{{$vacias->count()}}</strong> mesas vacías.
+          </p> 
                   {{-- <p>
                     @foreach ($estudiantes as $estudiante)
                       {{$estudiante->id}}: {{$estudiante->apellidos}},{{$estudiante->nombre}};<br> 
@@ -54,10 +60,10 @@
           <summary>Cambiar mesas vacías</summary>
           <div class=""><!-- Cambiar mesas vacías  -->
             <label class="d_inline" for="cambiarMesasVacias">Columna, fila</label>
-            <textarea name="cambiarMesasVacias" id="sencambiarMesasVacias" class="d_block" rows="1" placeholder="1,3;3,2"></textarea>
+            <textarea name="cambiarMesasVacias" id="cambiarMesasVacias" class="d_block" rows="1" placeholder="1,3;3,2"></textarea>
           </div> 
           <div class="mt-1">
-            <small class="ejemplo"><strong>Esquema: columna, fila</strong> donde 1 es la columna 1 de la izquierda y 3 es la fila 3 desde atrás</small>
+            <small class="ejemplo"><strong>Esquema: columna, fila</strong> donde 1 es la primera columna de la izquierda y 3 es la tercera fila desde delante.</small>
           </div>
         </details>
         <details class="mt-2">
@@ -66,32 +72,37 @@
             <label class="d_inline" for="sentarEstudiantes"></label>
             <textarea name="sentarEstudiantes" id="sentarEstudiantes" class="d_block" rows="1" placeholder="1,2,3,4,5,6,7,8,9"></textarea>
           </div>
-          <div class="mt-1">
-            <small class="ejemplo">Cada estudiante es un número del <strong>1 </strong> al <strong> {{$estudiantes->count()}} </strong><strong>Escribe una lista de números separados por comas.</strong></small>
+          <div class="mt-1 px-2">
+            <small class="ejemplo">Ordena tus estudiantes escribiendo los números del <strong>1 </strong> al <strong> {{count($ids_estudiante)}} </strong></small><small> <strong>separados por comas</strong></small>
           </div>
 
-          <div class= "mt-4"><!-- Ayuda  -->
+          <div class= "mt-2"><!-- Ayuda  -->
             <details class="mt-2">
-            <summary>Ver más:</summary>
+           {{--  <summary>Ver más:</summary> --}}
             <p class="mt-2"></p>
-            <div class="destacado py-2">
-              <p class="py-2 text-left">
-                @for($i = 1; $i <= $estudiantes->count(); $i++)
-                  Nº <kbd>coma</kbd>
+            <div class="destacado text-center py-2">
+              <p class="py-2 ">
+                @for($i = 1; $i <= count($ids_estudiante); $i++)
+                  @if($i<10) {{$i='0'.$i}}
+                  @else {{$i=''.$i}}
+                  @endif 
+                    <kbd> ,</kbd>&nbsp;
                   @if($i%$aula->num_columnas == 0)<br>
                   @endif 
                 @endfor
               </p>
             </div>  
-            <p class="mt-2">en este plano del aula visto <strong>desde la pizarra</strong>.</p>
+            <small class="mt-2">Ordenados <strong>por lista</strong> se verían así <strong>desde la pizarra</strong> .</small>
             <details class="mt-2">
-              <summary>¿Más?</summary>
-              <p class="mt-2">Copia y pega esta lista:</p>
-              <p class="mt-2 text-center">{{$estudiantes->count()}}
-                @for($i = $estudiantes->count()-1; $i > 0; $i--),{{$i}}@endfor
-              </p>
+              <summary>Copia y pega esta lista</summary>
+              {{-- <p class="mt-2 px-2">:</p> --}}
+              <div class="mt-1 px-2">
+              <small class="mt-2 text-center">{{count($ids_estudiante)}}
+                @for($i = count($ids_estudiante)-1; $i > 0; $i--), {{$i}}@endfor
+              </small>
+              </div>
             </details>
-          </details>
+          {{-- </details> --}}
         </div>
 
       </div>
